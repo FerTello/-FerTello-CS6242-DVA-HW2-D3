7af9a703abe7a5c6723641f7ee500b2153da834f
Fernanda Tello <Fernanda@Fernandas-MacBook-Pro.local>
2018-10-06 04:14:36 -0500

    colored US map with legend

--- Q6/code.js
+++ Q6/code.js
@@ -14,56 +14,85 @@
       path = d3.geo.path()
         .projection(projection);
 
-/*
-  var percentage = d3.map({});
-    var educated = d3.map({});
-*/
+    var stateIdMap = d3.map({});
+
     var q = d3.queue();
         q.defer(d3.json, "us.json")
-        q.defer(d3.csv, "data/state-population.csv")
         q.defer(d3.csv, "education.csv")
         q.defer(d3.csv, "education_details.csv")
-        q.await(function (err, US, percentage, educated){
+        q.await(function (err, US, eduPct, details){
 
-          //console.log(population);
-        //  console.log(percentage);
-          //console.log(educated);
+
+        var pctMap = eduPct.map(function(entry){
+          return {
+            id: entry.id,
+            county: entry.name,
+            percent: entry.percent_educated
+          }
+        });
 /*
-          var edu_prof = svg.append("g")
-                  .attr("class", "edu_prof")
-                  .selectAll("g")
-                  .data(percentage)
-                  .enter()
-                  .append("g");
+        pctMap.forEach(function(entry){
+          console.log(entry.county, entry.percent);
+        });
 */
-          var states = svg.append("g")
-                  .attr("class", "states")
-                  .selectAll("g")
-                  .data(topojson.feature(US, US.objects.states).features)
-                  .enter()
-                  .append("g");
-/*
-              populations = prepare.populations(populations);
+        //console.log(eduPct);
 
-          var quantize = d3.scale.quantize()
-                  .domain(d3.extent(_.values(percentage)))
-                  .range(d3.range(9).map(function(i) { return "q" + i + "-9-green"; }));
-*/
-              states.append("path")
-                  .attr("d", path);
+        var detMap = details.map(function(entry){
+          return {
+            id: entry.id,
+            professionals: entry.qualified_professionals,
+            highSchool: entry.high_school,
+            middleSchool: entry.middle_school_or_lower
+          }
+        });
 
-              states.append("path")
-                  .datum(topojson.mesh(US, US.objects.states,
-                                      function(a,b){ return a !== b; } ))
-                  .attr("class", "borders")
-                  .attr("d", path);
 
-              states.append("text")
-                  .text( function(d){ return stateIdMap.get(d.id) || d.id; })
-                  .attr({
-                    x: function(d){ return path.centroid(d)[0] || 0; },
-                    y: function(d){ return path.centroid(d)[1] || 0; }
-                  });
-        })
+        var color = d3.scale.threshold()
+              .domain([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+              .range(["#fff7ec","#fee8c8","#fdd49e","#fdbb84","#fc8d59","#ef6548","#d7301f","#b30000","#7f0000"]);
+
+        var quantize = d3.scale.quantize()
+                .domain([0, 100])
+                .range(d3.range(9).map(function(i) { return "q" + i + "-9-green"; }));
+
+                svg.append("g")
+                    .attr("class", "counties")
+                  .selectAll("path")
+                    .data(topojson.feature(US, US.objects.counties).features)
+                  .enter().append("path")
+                    .attr("d", path)
+                    .data(pctMap)
+                    .style("fill", function(d) { return color(d.percent); });
+
+            // Legend Stuff
+            var y = d3.scale.linear()
+                .domain([0, 100])
+                .range([0,300]);
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .tickValues(color.domain())
+                .orient("right");
+
+            var g = svg.append("g")
+                .attr("class", "key")
+                .attr("transform", "translate(920, 165)")
+                .call(yAxis);
+              g.selectAll("rect")
+               .data(color.range().map(function(d, i) {
+                   return {
+                       y0: i ? y(color.domain()[i - 1]) : y.range()[0],
+                       y1: i < color.domain().length ? y(color.domain()[i]) : y.range()[1],
+                       z: d
+                   };
+               }))
+               .enter().append("rect")
+                   .attr("width", 8)
+                   .attr("y", function(d) { return d.y0; })
+                   .attr("height", function(d) { return d.y1 - d.y0; })
+                   .style("fill", function(d) { return d.z; });
+
+
+        });
 
 })();
